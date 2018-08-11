0f1a70aa17f4467ad6f94f0994ef726f45ca6006
Alain Dupuis <adupuis2@unb.ca>
2018-08-11 20:41:26 -0300

    Finished lesson #5 – https://www.youtube.com/watch?v=k3bRdvmTdwQ

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    let name = "Alain";
+    let headerStyle = {
+      "color": "#6ef7c8",
+      "font-size": "50px"
+    };
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 style={headerStyle} className="App-title">
+            Hi there {name}!
+          </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
