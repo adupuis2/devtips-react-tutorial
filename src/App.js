35bcc0abb9b01001c2c725319356f8255b8eb8a9
Alain Dupuis <adupuis2@unb.ca>
2018-08-11 21:28:20 -0300

    Finished lesson #6 – https://www.youtube.com/watch?v=jt4jYHKBsvU

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-let defaultTextColor = "#6ef7c8";
 let defaultStyle = {
-  color: defaultTextColor,
+  color: "#6ef7c8",
 };
 
 class Aggregate extends Component {
@@ -49,7 +48,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style={{ ...defaultStyle, "font-size": "54px" }}>Title</h1>
         <Aggregate />
         <Aggregate />
         <Filter />
