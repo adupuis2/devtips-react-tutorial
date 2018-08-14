b27b6f5c76f89aa5e364771d834d0531081b65a2
Alain Dupuis <adupuis2@unb.ca>
2018-08-13 21:34:19 -0300

    Changed playlist image height to 60px

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class Playlist extends Component {
     let playlist = this.props.playlist;
     return (
       <div style={{ ...defaultStyle, width: "25%", display: "inline-block" }}>
-        <img src={playlist.imageUrl} style={{width: "170px"}}/>
+        <img src={playlist.imageUrl} style={{height: "60px"}}/>
         <h3>{playlist.name}</h3>
         <ul>
           {
