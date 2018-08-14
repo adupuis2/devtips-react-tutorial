5b9837cdab09f607893071bb760f026379c002e4
Alain Dupuis <adupuis2@unb.ca>
2018-08-14 17:05:32 -0300

    Cleaned up a lot of spaghetti (still not great). Added some error handling. Moved components to external module.

--- src/App.js
+++ src/App.js
@@ -1,83 +1,83 @@
 import React, { Component } from "react";
+import {
+  defaultStyle,
+  PlaylistCounter,
+  HoursCounter,
+  Filter,
+  Playlist
+} from "./Components";
 import qs from "query-string"; // Used to parse access token
 import "./App.css";
 
-// Default text color. Yay pink!
-let defaultStyle = {
-  color: "#f9499e",
-};
-
-// The number of playlists being displayed
-class PlaylistCounter extends Component {
-  render() {
-    return (
-      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
-        <h2>{this.props.playlists.length} playlists</h2>
-      </div>
-    );
-  }
+// Retrieve a promise for the playlists' json
+function getHrefJsonPromise(accessToken, href) {
+  let got = fetch(href,
+    {
+      headers: { "Authorization": "Bearer " + accessToken }
+    }).then(response => response.json());
+  return got;
 }
 
-// The total hours of playtime for all displayed playlists
-class HoursCounter extends Component {
-  render() {
-    // Combine all songs from all playlists into an array
-    let allSongs = this.props.playlists.reduce((acc, eachPlaylist) => {
-      return acc.concat(eachPlaylist.songs);
-    }, []);
-
-    // Combine the playtime from all songs into a single number
-    let hours = allSongs.reduce((acc, song) => {
-      return (acc + song.duration);
-    }, 0);
-
-    // Songs are measured in milliseconds. Converts playtime to hours.
-    hours /= 1000; // convert to seconds
-    hours /= 60; // convert to minutes
-    hours /= 60; // convert to hours.
-    hours = Math.round(hours*100) / 100; // round to two decimal places
-
-    return (
-      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
-        <h2>{hours} hours</h2>
-      </div>
-    );
-  }
+// Get user details for user name
+// and sets the associated app state
+function FetchAndSetUsername(accessToken, context) {
+  getHrefJsonPromise(accessToken, "https://api.spotify.com/v1/me")
+    .then(data => {
+      context.setState({
+        user: {
+          name: data.error ? "ERROR" : data.display_name
+        }
+      });
+    });
 }
 
-// The textbox for filtering playlists by name
-class Filter extends Component {
-  render() {
-    return (
-      <div style={defaultStyle}>
-        <img />
-        <input type="text" onChange={event => this.props.onTextChange(event.target.value)}/>
-      </div>
-    );
-  }
-}
+// Get user's playlist details
+// and sets the associated app state
+function FetchAndSetPlaylistDetails(accessToken, context) {
 
-// Each individual playlist
-class Playlist extends Component {
-  render() {
-    // Shorten variable name for clarity
-    let playlist = this.props.playlist;
-
-    return (
-      <div style={{ ...defaultStyle, width: "25%", display: "inline-block" }}>
-        <img src={playlist.imageUrl} style={{width: "120px"}}/>
-        <h3>{playlist.name}</h3>
-        <ol>
-          {playlist.songs.slice(0, 3).map(song =>
-            <li>
-              {song.name}
-            </li>
-          )
-          }
-        </ol>
-      </div>
+  function getAllTracksForPlaylists(playlistJson) {
+    let playlists = playlistJson.items;
+    let trackDataPromises = playlists.map(playlist =>
+      getHrefJsonPromise(accessToken, playlist.tracks.href)
     );
+    let allTracksListsPromises = Promise.all(trackDataPromises);
+    let playlistsPromise = allTracksListsPromises
+      .then(tracksLists => {
+        tracksLists.forEach((trackList, i) => {
+          playlists[i].trackList =
+            trackList.items.map(item => item.track);
+        });
+        return playlists;
+      });
+    return playlistsPromise;
   }
+
+  function setStateData(playlistsPromise) {
+    playlistsPromise.then(playlists => context.setState({
+      // Populate playlist state data
+      playlists: playlists.map(item => {
+        return {
+          name: item.name,
+          imageUrl: item.images[0].url,
+          songs: item.trackList.map(track => ({
+            name: track.name,
+            duration: track.duration_ms,
+          }))
+        };
+      })
+    }));
+  }
+
+  let jsonPromise = getHrefJsonPromise(accessToken,
+    "https://api.spotify.com/v1/me/playlists");
+  jsonPromise.then(dat => {
+    if (dat.error)
+      context.setState({error: true});
+    else {
+      let playlistData = getAllTracksForPlaylists(dat);
+      setStateData(playlistData);
+    }
+  });
 }
 
 class App extends Component {
@@ -97,57 +97,11 @@ class App extends Component {
     // Fail gracefully
     if (!accessToken)
       return;
-    
-    // Get user details for user name
-    fetch("https://api.spotify.com/v1/me", {
-      headers: { "Authorization": "Bearer " + accessToken }
-    })
-      .then(response => response.json())
-      .then(data => this.setState({
-        user: {
-          name: data.display_name
-        }
-      }));
 
-    // Get user's playlist details
-    fetch("https://api.spotify.com/v1/me/playlists", {
-      headers: { "Authorization": "Bearer " + accessToken }
-    })
-      .then(response => response.json())
-      .then(playlistData => {
-        let playlists = playlistData.items;
-        let trackDataPromises = playlists.map(playlist => {
-          let songPromises = fetch(playlist.tracks.href,
-            { headers: { "Authorization": "Bearer " + accessToken } });
-          let songDataPromises = songPromises
-            .then(response => response.json());
-          return songDataPromises;
-        });
-        let allTracksListsPromises = Promise.all(trackDataPromises);
-        let playlistsPromise = allTracksListsPromises
-          .then(tracksLists => {
-            tracksLists.forEach((trackList, i) => {
-              playlists[i].trackList = 
-              trackList.items.map(item => item.track);
-            });
-            return playlists;
-          });
-        return playlistsPromise;
-      })
-      .then(playlists => this.setState({
-        // Populate playlist state data
-        playlists: playlists.map(item => {
-          return {
-            name: item.name,
-            imageUrl: item.images[0].url,
-            songs: item.trackList.map(track => ({
-              name: track.name,
-              duration: track.duration_ms,
-            }))
-          };
-        })
-      }));
+    FetchAndSetUsername(accessToken, this);
+    FetchAndSetPlaylistDetails(accessToken, this);
   }
+
 
   render() {
     // Ensure we have playlist data to filter
@@ -164,9 +118,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        {this.state.user
+        {(this.state.user && !this.state.error)
 
-          // If user data exists, draw page.
+          // If user data exists and there are no errors, draw page.
           // Must be wrapped in div – can only return a single tag.
           ? <div>
             <h1 style={{ ...defaultStyle, "fontSize": "54px" }}>
@@ -175,7 +129,7 @@ class App extends Component {
             <PlaylistCounter playlists={playlistsToRender} />
             <HoursCounter playlists={playlistsToRender} />
             <Filter onTextChange={text => this.setState({ filterString: text })} />
-            {playlistsToRender.map(playlist => <Playlist playlist={playlist} />)}
+            {playlistsToRender.map(playlist => <Playlist playlist={playlist} key={playlist.name} />)}
           </div>
 
           // User data doesn't exist. Prompt to login
---
+++ src/Components.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+
+// Default text color. Yay pink!
+let defaultStyle = {
+  color: "#f9499e",
+};
+
+class PlaylistCounter extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+}
+
+// The total hours of playtime for all displayed playlists
+class HoursCounter extends Component {
+  render() {
+    // Combine all songs from all playlists into an array
+    let allSongs = this.props.playlists.reduce((acc, eachPlaylist) => {
+      return acc.concat(eachPlaylist.songs);
+    }, []);
+
+    // Combine the playtime from all songs into a single number
+    let hours = allSongs.reduce((acc, song) => {
+      return (acc + song.duration);
+    }, 0);
+
+    // Songs are measured in milliseconds. Converts playtime to hours.
+    hours /= 1000; // convert to seconds
+    hours /= 60; // convert to minutes
+    hours /= 60; // convert to hours.
+    hours = Math.round(hours*100) / 100; // round to two decimal places
+
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>{hours} hours</h2>
+      </div>
+    );
+  }
+}
+
+// The textbox for filtering playlists by name
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <input type="text" onChange={event => this.props.onTextChange(event.target.value)}/>
+      </div>
+    );
+  }
+}
+
+// Each individual playlist
+class Playlist extends Component {
+  render() {
+    // Shorten variable name for clarity
+    let playlist = this.props.playlist;
+
+    return (
+      <div style={{ ...defaultStyle, width: "25%", display: "inline-block" }}>
+        <img src={playlist.imageUrl} style={{width: "120px"}} alt={playlist.name} />
+        <h3>{playlist.name}</h3>
+        <ol>
+          {playlist.songs.slice(0, 3).map(song =>
+            <li key={song.name}>
+              {song.name}
+            </li>
+          )
+          }
+        </ol>
+      </div>
+    );
+  }
+}
+
+export {
+  defaultStyle,
+  PlaylistCounter,
+  HoursCounter,
+  Filter,
+  Playlist
+};
